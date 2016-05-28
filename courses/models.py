5a0042f52402bce26b1c23fa81dc68f77c437051
Cody Wilson <cheex11@gmail.com>
2016-05-28 18:18:05 -0400

    added an app - made migrations.

---
+++ courses/models.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+	create_at = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=255)
+	description = models.TextField()
