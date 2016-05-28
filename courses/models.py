181efdf4c65581e40a895ffc870db3f7052217a5
Cody Wilson <cheex11@gmail.com>
2016-05-28 19:16:31 -0400

    going through tutorials

--- courses/models.py
+++ courses/models.py
@@ -7,3 +7,6 @@ class Course(models.Model):
 	create_at = models.DateTimeField(auto_now_add=True)
 	title = models.CharField(max_length=255)
 	description = models.TextField()
+
+	def __str__(self):
+		return self.title
